86634c1b309e597a867a48d0ebcec605cd708624
Rodrigo Britez Junior <rjunior@topconsuite.com>
2022-09-11 13:33:36 -0300

    inject service dependency on controller

--- ControllerService.cs
+++ ControllerService.cs
@@ -1,23 +1,27 @@
 using FroniusIntegration.Command;
 using FroniusIntegration.Entities;
 using FroniusIntegration.Handlers;
+using FroniusIntegration.Services;
 using Hangfire;
 
 namespace FroniusIntegration;
 
 public class ControllerService
 {
+  private readonly GetFroniusApi _getFroniusApi;
   private readonly PowerEnergyHandler _powerEnergyHandler;
 
-  public ControllerService(PowerEnergyHandler powerEnergyHandler)
+  public ControllerService(PowerEnergyHandler powerEnergyHandler, GetFroniusApi getFroniusApi)
   {
     _powerEnergyHandler = powerEnergyHandler;
+    _getFroniusApi = getFroniusApi;
   }
 
   [DisableConcurrentExecution(0)]
   [AutomaticRetry(Attempts = 0)]
   public async Task ProcessEventsAsync()
   {
+    var result = await _getFroniusApi.GetConcretingAsync();
     var generation = new Generation(1, 5, DateTime.Now, DateTime.Now);
     BackgroundJob.Enqueue(() => _powerEnergyHandler.HandleAsync(new IntegrateGenerationCommand(generation)));
   }
